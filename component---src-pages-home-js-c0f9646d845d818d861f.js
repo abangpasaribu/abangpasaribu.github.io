9c0db7f77580dd0c253836fbb1b3d2a7a5b00b5a
Abang Pasaribu <abangpasaribu80@gmail.com>
2021-07-26 13:44:41 +0700

    Home added

---
+++ component---src-pages-home-js-c0f9646d845d818d861f.js
@@ -0,0 +1,2 @@
+(self.webpackChunkblog=self.webpackChunkblog||[]).push([[79],{516:function(e,n,u){"use strict";u.r(n),u.d(n,{default:function(){return l}});var t=u(294);function l(){return t.createElement("h1",null,"I am home")}}}]);
+//# sourceMappingURL=component---src-pages-home-js-c0f9646d845d818d861f.js.map
